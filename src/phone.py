8e200ab1ede62c84bb04005899ed3ee4d9919612
sergei valdemarov <valdemarovsergei@gmail.com>
2023-06-14 23:35:08 +0300

    add file phone 2

--- src/phone.py
+++ src/phone.py
@@ -9,11 +9,3 @@ class Phone(Item):
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
-
-    def __str__(self):
-        return f"{self.name}"
-
-    def __add__(self, other):
-        if not isinstance(other, Item):
-            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
-        return self.quantity + other.quantity
